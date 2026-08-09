847144a64ce40a8e10dccd28bcbb051444cfaa96
TeddyGandon <teddy@coretizone.com>
2026-08-09 08:43:28 +0000

    feat: feature flags for dates

--- src/data/flags.js
+++ src/data/flags.js
@@ -1,5 +1,7 @@
+const today = new Date().toISOString().slice(0, 10);
+
 export const flags = {
-  displayCertifications: false,
-  displayAllArticles: false,
-  displayArticlesTags: false,
+  displayAllArticles: false, // Only for local env
+  displayCertifications: today >= '2026-10-01', // Will get the certifications at this date
+  displayArticlesTags: today >= '2026-09-14', // Display at the third article
 };
