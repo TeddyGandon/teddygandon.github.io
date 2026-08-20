0482969bf4cf0364e6e66191eee56432ef88b3d7
TeddyGandon <teddy@coretizone.com>
2026-08-20 21:28:02 +0530

    Update image paths in linkedin.js

--- src/data/linkedin.js
+++ src/data/linkedin.js
@@ -6,19 +6,19 @@ export const linkedinPosts = [
     date: '2026-07-20',
     text: `Who says tackling tech debt and security compliance can't be fun? 🚀 We just wrapped up our 3-day Summer Activities with all of our engineering squads!`,
     url: 'https://www.linkedin.com/posts/teddygandon_engineeringleadership-interculturalmanagement-share-7491095179622371328-ibKk/',
-    image: 'https://media.licdn.com/dms/image/v2/D4E22AQHpL_zAaj_NCg/feedshare-shrink_1280/B4EZ_W3SYCJUAM-/0/1786016245964?e=1787788800&v=beta&t=m7jvBxvbbh_XL88H_UpXYNAj48oiL0dkKyMQdE8ABCE',
+    image: './1780570158087.jpeg',
   },
   {
     date: '2026-07-20',
     text: `🇮🇳✈️🇫🇷 Getting back in France after an incredible month in India! Spending the last four weeks immersed in India's tech ecosystem has been sooo challenging, but also so refreshing !`,
     url: 'https://www.linkedin.com/posts/teddygandon_kochi-bengaluru-techinnovation-activity-7473332350253289472-m1_Q',
-    image: 'https://media.licdn.com/dms/image/v2/D5622AQFPVPB4uemMfw/feedshare-shrink_800/B56Z7aTz6fJQAg-/0/1781779087254?e=1787184000&v=beta&t=sQyTLZoYoHl1BCwSXud8lkV7XLoQHWqUE9o5c5ap1Z4',
+    image: './1781779087254.jpeg',
   },
   {
     date: '2026-07-05',
     text: `🎤 I just conducted two talks at Brototype, hashtag#Kochi, India about problems solving methods and feedback loop! Brototype is a bootcamp built on hands-on, peer-driven learning, designed to develop job-ready engineers through real-world practice and strong mentorship.`,
     url: 'https://www.linkedin.com/posts/teddygandon_kochi-kochitech-mentorship-activity-7468262931688751105-aLbe',
-    image: 'https://media.licdn.com/dms/image/v2/D5622AQFBmTawgNNY-w/feedshare-shrink_800/B56Z6SQHJGHcAc-/0/1780570158087?e=1787184000&v=beta&t=PbDnRtVy7Bw6af6tupTHVwiCEGQ5hDscSYLwymqCb9o',
+    image: './1786016246035.jpeg',
   },
   {
     date: '2026-05-29',
